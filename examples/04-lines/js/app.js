e548fd4f1e6bdd26fa84a0c751e480f629370cd9
burodepeper <david@burodepeper.nl>
2015-12-26 15:43:08 +0100

    Add SpectrumAnalyzer to example-04

--- examples/04-lines/js/app.js
+++ examples/04-lines/js/app.js
@@ -20,7 +20,16 @@ App = {
 SpectrumAnalyzer = (function(superClass) {
   extend(SpectrumAnalyzer, superClass);
 
+  SpectrumAnalyzer.prototype.counter = 0;
+
+  SpectrumAnalyzer.prototype.points = [];
+
+  SpectrumAnalyzer.prototype.min = 0;
+
+  SpectrumAnalyzer.prototype.max = 42;
+
   function SpectrumAnalyzer() {
+    var center, i, increment, j, numberOfPoints, point, ref, x, y;
     SpectrumAnalyzer.__super__.constructor.call(this);
     this.pane = new Pane();
     this.pane.setCSS({
@@ -29,7 +38,40 @@ SpectrumAnalyzer = (function(superClass) {
       bottom: 1,
       height: 43
     });
+    this.width = this.pane.getWidth();
+    this.height = this.pane.getHeight();
+    numberOfPoints = Math.ceil(this.width / 21);
+    increment = this.width / (numberOfPoints - 1);
+    this.path = new Path();
+    this.pane.addChild(this.path);
+    x = 0;
+    center = 21;
+    for (i = j = 0, ref = numberOfPoints - 1; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+      y = center + Math.floor(10 - Math.random() * 20);
+      point = new Point(x, y);
+      this.path.addPoint(point);
+      x += increment;
+      this.points.push(point);
+    }
   }
+
+  SpectrumAnalyzer.prototype.update = function() {
+    var j, len, point, r, ref, y;
+    if ((this.counter % 2) === 1) {
+      ref = this.points;
+      for (j = 0, len = ref.length; j < len; j++) {
+        point = ref[j];
+        y = point.y;
+        r = getRandomInt(this.min, this.max);
+        if (r < y) {
+          point.y -= 1;
+        } else if (r > y) {
+          point.y += 1;
+        }
+      }
+    }
+    return this.counter++;
+  };
 
   return SpectrumAnalyzer;
 
--- examples/04-lines/js/src/SpectrumAnalyzer.coffee
+++ examples/04-lines/js/src/SpectrumAnalyzer.coffee
@@ -1,7 +1,60 @@
 class SpectrumAnalyzer extends Controller
 
+  counter: 0
+  points: []
+  min: 0
+  max: 42
+
   constructor: ->
     super()
 
     @pane = new Pane()
     @pane.setCSS({ left:1, right:1, bottom:1, height:43 })
+    @width = @pane.getWidth()
+    @height = @pane.getHeight()
+
+    # TODO
+    # Decorate the pane
+    # - Dark gray background
+    # - Less dark gray line at vertical center
+    # line = new Line()
+    # @pane.addChild(line)
+    # line.from(new Point(0, (@height - 1) / 2)).to(new Point(@width - 1, (@height - 1) / 2))
+
+    numberOfPoints = Math.ceil(@width / 21)
+    increment = @width / (numberOfPoints - 1)
+    @path = new Path()
+    @pane.addChild(@path)
+    x = 0
+    center = 21
+
+    for i in [0 .. numberOfPoints - 1]
+      y = center + Math.floor(10 - Math.random() * 20)
+      point = new Point(x, y)
+      @path.addPoint(point)
+      x += increment
+      @points.push(point)
+
+  update: ->
+
+    # Shift point.y one place horizontally at 6 fps
+    # and generate a new y for the last one
+    # if (@counter % 10) is 1
+    #   for i in [@points.length - 1 .. 0]
+    #     point = @points[i]
+    #     if i
+    #       point.y = @points[i - 1].y
+    #     else
+    #       point.y = 21 + Math.floor(10 - Math.random() * 20)
+    # @counter++
+
+    # Randomly change point.y at 30 fps
+    if (@counter % 2) is 1
+      for point in @points
+        y = point.y
+        r = getRandomInt(@min, @max)
+        if r < y
+          point.y -= 1
+        else if r > y
+          point.y += 1
+    @counter++
